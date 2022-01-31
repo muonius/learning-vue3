b3a7cf9f289cfa47b908cc9fcd64d6a307421711
Yang Zhao <42701448+muonius@users.noreply.github.com>
2022-01-31 11:41:27 -0500

    update

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        handleEvent(e, data) {
+            //e argument will always be the first argument
+            console.log(e, e.type)
+            //access properties of e in the case of click and alt key is pressed at the same time
+            if (data) console.log(data)
+        },
+        handleMouseMove(e) {
+
+            this.x = e.offsetX
+            this.y = e.offsetY
+
+
         }
     }
     // methods: {
