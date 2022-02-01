7591653b0768035837bbc1c9c78636903a1ad355
Yang Zhao <42701448+muonius@users.noreply.github.com>
2022-01-31 19:32:34 -0500

    update

--- app.js
+++ app.js
@@ -3,16 +3,40 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0,
+            url: 'http://www.thenetninjia.co.uk',
+            books: [
+                {
+                    title: 'name of the wind',
+                    author: 'patrick rothfuss',
+                    img: './assets/1.jpg',
+                    isFav: true
+                },
+                {
+                    title: 'name of the wind2',
+                    author: 'patrick rothfuss2',
+                    img: './assets/2.jpg',
+                    isFav: false
+                },
+                {
+                    title: 'name of the wind3',
+                    author: 'patrick rothfuss3',
+                    img: './assets/3.jpg',
+                    isFav: true
+                }
+            ]
+            // title: 'The Final Empire',
+            // author: 'Brandon Sanderson',
+            // age: 45,
+            // x: 0,
+            // y: 0,
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav;
         },
         handleEvent(e, data) {
             //e argument will always be the first argument
@@ -21,18 +45,22 @@ const app = Vue.createApp({
             if (data) console.log(data)
         },
         handleMouseMove(e) {
-
             this.x = e.offsetX
             this.y = e.offsetY
-
-
         }
-    }
+    },
     // methods: {
     //     changeTitle(title) {
     //         this.title = title
     //     }
     // }
+    computed: {
+        //depends on the data above
+        filteredBooks() {
+            return this.books.filter(book => book.isFav)
+        }
+    }
+
 })
 
 app.mount('#app')
