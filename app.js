d67f4eebd60a3fbd9423e085423dac88646d8897
Yang Zhao <freda.y.zhao@gmail.com>
2022-07-18 20:51:37 -0400

    update

--- app.js
+++ app.js
@@ -1,67 +1,26 @@
+// 1. Create Vue app
 const app = Vue.createApp({
-    //this is the root component of the app
-    data() {
-        return {
-            showBooks: true,
-            url: 'http://www.thenetninjia.co.uk',
-            books: [
-                {
-                    title: 'name of the wind',
-                    author: 'patrick rothfuss',
-                    img: './assets/1.jpg',
-                    isFav: true
-                },
-                {
-                    title: 'name of the wind2',
-                    author: 'patrick rothfuss2',
-                    img: './assets/2.jpg',
-                    isFav: false
-                },
-                {
-                    title: 'name of the wind3',
-                    author: 'patrick rothfuss3',
-                    img: './assets/3.jpg',
-                    isFav: true
-                }
-            ]
-            // title: 'The Final Empire',
-            // author: 'Brandon Sanderson',
-            // age: 45,
-            // x: 0,
-            // y: 0,
-        }
+  //4. must create a data(){} and return a function
+  data() {
+    return {
+      showBooks: true,
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+    };
+  },
+  methods: {
+    changeTitle(title) {
+      //5. this points to the main component
+      this.title = title;
     },
-    methods: {
-        toggleShowBooks() {
-            this.showBooks = !this.showBooks;
-        },
-        toggleFav(book) {
-            book.isFav = !book.isFav;
-        },
-        handleEvent(e, data) {
-            //e argument will always be the first argument
-            console.log(e, e.type)
-            //access properties of e in the case of click and alt key is pressed at the same time
-            if (data) console.log(data)
-        },
-        handleMouseMove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
-        }
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
-    // methods: {
-    //     changeTitle(title) {
-    //         this.title = title
-    //     }
-    // }
-    computed: {
-        //depends on the data above
-        filteredBooks() {
-            return this.books.filter(book => book.isFav)
-        }
-    }
+    handleEvent() {},
+  },
+});
 
-})
-
-app.mount('#app')
-//take app instance and mount it to the DOM with element with an id #app
+//3. Create a root component object
+//2. Mount to element, only control this element
+app.mount("#app");
